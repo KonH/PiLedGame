7d915a32b8b3467a26c0877a66d637de44bb5ba8
KonH <konh@yandex.ru>
2019-09-24 18:34:58 +0700

    Fix non-tracked damage issue

--- Systems/DamageSystem.cs
+++ Systems/DamageSystem.cs
@@ -5,15 +5,22 @@ using PiLedGame.State;
 
 namespace PiLedGame.Systems {
 	public sealed class DamageSystem : ISystem {
-		Dictionary<Point2D, DamageComponent> _damageByPosition = new Dictionary<Point2D, DamageComponent>();
+		Dictionary<Point2D, List<DamageComponent>> _damageByPosition = new Dictionary<Point2D, List<DamageComponent>>();
 
 		public void Update(GameState state) {
 			_damageByPosition.Clear();
 			foreach ( var (_, position, damage) in state.Entities.Get<PositionComponent, DamageComponent>() ) {
-				_damageByPosition[position.Point] = damage;
+				if ( !_damageByPosition.TryGetValue(position.Point, out var damages) ) {
+					damages = new List<DamageComponent>();
+					_damageByPosition.Add(position.Point, damages);
+				}
+				damages.Add(damage);
 			}
 			foreach ( var (_, position, health) in state.Entities.Get<PositionComponent, HealthComponent>() ) {
-				if ( _damageByPosition.TryGetValue(position.Point, out var damage) ) {
+				if ( !_damageByPosition.TryGetValue(position.Point, out var damages) ) {
+					continue;
+				}
+				foreach ( var damage in damages ) {
 					if ( damage.Layer == health.Layer ) {
 						continue;
 					}
