a61458eb4cd3366058ac49bc3c8ebee249020294
KonH <konh@yandex.ru>
2019-12-03 21:02:06 +0700

    Samples

--- SoftwareRender.Core/Systems/DirectRenderSystem.cs
+++ SoftwareRender.Core/Systems/DirectRenderSystem.cs
@@ -1,11 +1,22 @@
 using SimpleECS.Core.Common;
 using SimpleECS.Core.State;
 using SimpleECS.Core.Systems;
+using SoftwareRender.Core.Render;
 
 namespace SoftwareRender.Core.Systems {
 	public sealed class DirectRenderSystem : ISystem {
 		public void Update(GameState state) {
-			state.Graphics.Frame.ChangeAt(new Point2D(16, 16), Color.Red);
+			var frame = state.Graphics.Frame;
+
+			frame.DrawScreenPoint(0, 0, Color.Red);
+			frame.DrawScreenPoint(31, 0, Color.Red);
+			frame.DrawScreenPoint(0, 31, Color.Red);
+			frame.DrawScreenPoint(31, 31, Color.Red);
+
+			frame.DrawScreenLine(1, 1, 30, 30, Color.Green);
+			frame.DrawScreenLine(30, 1, 1, 30, Color.Green);
+			frame.DrawScreenLine(4, 0, 4, 31, Color.Green);
+			frame.DrawScreenLine(0, 27, 31, 27, Color.Green);
 		}
 	}
 }
