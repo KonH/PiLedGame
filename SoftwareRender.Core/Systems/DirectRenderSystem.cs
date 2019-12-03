add046b71b9d0fa9d917aa8359e822e7af20aed9
KonH <konh@yandex.ru>
2019-12-03 22:46:16 +0700

    Simple rendering direct into screen coordinates

--- SoftwareRender.Core/Systems/DirectRenderSystem.cs
+++ SoftwareRender.Core/Systems/DirectRenderSystem.cs
@@ -7,20 +7,19 @@ namespace SoftwareRender.Core.Systems {
 	public sealed class DirectRenderSystem : ISystem {
 		public void Update(GameState state) {
 			var frame = state.Graphics.Frame;
+			var points = new[] { new Point3D(5, 5, 5) };
+			DrawPoints(frame, points, Color.Red);
+		}
 
-			frame.DrawScreenPoint(0, 0, Color.Red);
-			frame.DrawScreenPoint(31, 0, Color.Red);
-			frame.DrawScreenPoint(0, 31, Color.Red);
-			frame.DrawScreenPoint(31, 31, Color.Red);
+		static void DrawPoints(Frame frame, Point3D[] points, Color color) {
+			foreach ( var point in points ) {
+				var (x, y) = ToScreenPoint(point);
+				frame.DrawScreenPoint(x, y, color);
+			}
+		}
 
-			frame.DrawScreenLine(1, 1, 30, 30, Color.Green);
-			frame.DrawScreenLine(30, 1, 1, 30, Color.Green);
-			frame.DrawScreenLine(4, 0, 4, 31, Color.Green);
-			frame.DrawScreenLine(0, 27, 31, 27, Color.Green);
-
-			frame.DrawScreenLineSmooth(1, 5, 20, 10, Color.Firebrick);
-
-			frame.DrawScreenCircle(20, 20, 8, Color.Indigo);
+		static Point2D ToScreenPoint(Point3D point) {
+			return new Point2D((int)point.X, (int)point.Y);
 		}
 	}
 }
